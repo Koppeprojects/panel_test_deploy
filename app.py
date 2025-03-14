bfb4720bdc4e61976f02365add38654379e31821
Koppeprojects <50079389+Koppeprojects@users.noreply.github.com>
2025-03-14 16:22:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ pn.extension()
 
 def say_hello(name="World"):
     return f"Hello, {name}!"
+    
+button = pn.widgets.Button(name="Click Me", button_type="primary")
 
-pn.interact(say_hello, name="World").servable()
+def on_click(event):
+    button.name = "Clicked!"
+
+button.on_click(on_click)
+
+interact=pn.interact(say_hello, name="World")
+pn.Row(button,interact).servable()
